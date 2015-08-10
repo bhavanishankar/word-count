1545d4e54be90da25d7aaa92ec560879f35a6c5f
bhavanishankar <bhavanishankar.j@imaginea.com>
2015-08-10 21:33:58 +0530

    wordcount initial commit

---
+++ src/word-count.js
@@ -0,0 +1,18 @@
+var jbsApp = {};
+jbsApp.getWordCount = function(userStr) {
+    if(userStr.length <=0) return;
+    var wordsObj = getWordsObj(userStr);
+    for(var prop in wordsObj) {
+        if(wordsObj.hasOwnProperty(prop))
+        console.log(prop + ' : ' + wordsObj[prop].count);
+    }
+    function getWordsObj(str) {
+       var arr = str.split(' '), _wordsObj = {}, i = 0; arrLength = arr.length;
+        for(i; i<arrLength; i++) {
+            (_wordsObj[arr[i]]) ? _wordsObj[arr[i]].count++ : _wordsObj[arr[i]] = {count : 1};
+        }
+        return _wordsObj;
+    }
+}
+
+jbsApp.getWordCount('hootcode is fun. hootcode is for javascript and java');
