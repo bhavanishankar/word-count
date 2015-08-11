665bb10bc0da52bec13739ab0fe75b8f3654a232
bhavanishankar <bhavanishankar.j@imaginea.com>
2015-08-11 06:17:04 +0530

    fixing semicolon issue

--- src/word-count.js
+++ src/word-count.js
@@ -7,12 +7,12 @@ jbsApp.getWordCount = function(userStr) {
         console.log(prop + ' : ' + wordsObj[prop].count);
     }
     function getWordsObj(str) {
-       var arr = str.split(' '), _wordsObj = {}, i = 0; arrLength = arr.length;
+       var arr = str.split(' '), _wordsObj = {}, i = 0, arrLength = arr.length;
         for(i; i<arrLength; i++) {
             (_wordsObj[arr[i]]) ? _wordsObj[arr[i]].count++ : _wordsObj[arr[i]] = {count : 1};
         }
         return _wordsObj;
     }
-}
+};
 
 jbsApp.getWordCount('hootcode is fun. hootcode is for javascript and java');
